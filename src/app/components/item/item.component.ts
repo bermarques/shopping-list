9d9b5714632199ef3705887fff4aa3bf09f1dd28
Bernardo Marques <bernardohmarques@gmail.com>
2024-09-30 02:14:50 -0300

    create and edit item

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
 
   faPen = faPen;
-  faTrash = faTrash
+  faTrash = faTrash;
 
-  constructor() { }
+  constructor(private listaService: ListaDeCompraService) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
+  ngOnChanges(): void {}
+
+  editarItem() {
+    this.emitindoItemParaEditar.emit(this.item);
+  }
 }
